4d03a3481af2e4aa74018cd6495b9d7bfff6f1fa
Mohammedhussein12 <Mohammedhussein12>
2023-09-14 16:39:19 +0300

    finish notes bottom sheet

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 55,
+      decoration: BoxDecoration(
+        color: kPrimaryColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      width: MediaQuery.of(context).size.width,
+      child: const Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField(
-      {super.key, required this.hintText,this.maxLines = 1});
+  const CustomTextField({super.key, required this.hintText, this.maxLines = 1});
 
   final String hintText;
   final int maxLines;
 
   @override
   Widget build(BuildContext context) {
-    return TextField(maxLines: maxLines,
+    return TextField(
+      maxLines: maxLines,
       cursorColor: kPrimaryColor,
       decoration: InputDecoration(
         hintText: hintText,
