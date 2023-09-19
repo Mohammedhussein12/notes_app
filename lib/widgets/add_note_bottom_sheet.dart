99316a0da660f798e8baf619e0150988fc580305
Mohammedhussein12 <Mohammedhussein12>
2023-09-19 15:02:01 +0300

    create add note cubit

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/widgets/custom_button.dart';
-import 'package:notes_app/widgets/custom_text_field.dart';
 
 import 'add_note_form.dart';
 
