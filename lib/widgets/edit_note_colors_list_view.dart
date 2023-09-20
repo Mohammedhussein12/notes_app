da0cad86f806238664914418a1ff33a151da5f3c
Mohammedhussein12 <Mohammedhussein12>
2023-09-21 01:56:24 +0300

    done

---
+++ lib/widgets/edit_note_colors_list_view.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import '../models/note_model.dart';
+import 'color_list_view.dart';
+
+class EditNoteColorsList extends StatefulWidget {
+  const EditNoteColorsList({super.key, required this.note});
+
+  final NoteModel note;
+
+  @override
+  State<EditNoteColorsList> createState() => _EditNoteColorsListState();
+}
+
+class _EditNoteColorsListState extends State<EditNoteColorsList> {
+  late int currentIndex;
+
+  @override
+  void initState() {
+    currentIndex = kColors.indexOf(Color(widget.note.color));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 38 * 2,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 6.0),
+          child: GestureDetector(
+            onTap: () {
+              currentIndex = index;
+              widget.note.color = kColors[index].value;
+              setState(() {});
+            },
+            child: ColorItem(
+              isActive: index == currentIndex,
+              color: kColors[index],
+            ),
+          ),
+        ),
+        itemCount: kColors.length,
+      ),
+    );
+  }
+}
